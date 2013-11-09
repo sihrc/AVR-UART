4c6bf53ac4572d46b4e1f3ff1c4dd3c3e08c49bb
Derek Redfern <redfern.derek@gmail.com>
2013-11-08 20:21:38 -0500

    Added basic methods for using UART

--- main.c
+++ main.c
@@ -5,58 +5,40 @@
 #include <avr/interrupt.h>
 #include <stdlib.h>
 
-#define maxDataLength 8
-
-/* TEST CAN CODE - DEMO NODE 1
-        Listens for changes on INT0, which is connected to a momentary button.
-        When changes occur, an interrupt fires and it sends an appropriate message across the CAN bus.
-        If the pin is high, it sends 0xFF. If the pin is low, it sends 0x00.
+/* TEST CODE - UART
+        Initializes the UART controller and sends out sporadic debug messages.
 */
 
-// TODO: set up 2 more external interrupts
+// Set up the LIN/UART Controller
+// RX on 12
+// TX on 2
 
-// Set up external interrupts for INT0 for any logical change
-int initButton() {
-    EICRA = _BV(ISC00);
-    EIMSK = _BV(INT0);
-    return(0);
+// Scale down to 9600 baud (bits per sec)
+// LDIV[11..0] = ( 8,000,000 / 32 x BAUD ) - 1
+// 
+void initUART(void) {
+    LINCR = _BV(LENA) | _BV(LCONF1) | _BV(LCMD2) | _BV(LCMD0);
+    LINBRR = 0x0C; //0b00001100
 }
 
-// TODO: add 2 more ISRs for the other boards (+ the broadcast channel)
-
-// Interrupt routine for External Interrupt 0 (fires when pin 14 changes)
-// Reads the value of pin 14 and sends an appropriate CAN message.
-ISR(INT0_vect) {
-    char cSREG = SREG; //store SREG
-    // int val = PIND & _BV(PD6);
-    // uint8_t* msg = (uint8_t*)malloc(1*sizeof(uint8_t));
-    // if (val) {
-    //     *msg = 0xFF;
-    // } else {
-    //     *msg = 0x00;
-    // }
-    // sendCANmsg(NODE_broadcast, MSG_demoMsg, msg, 1);
-    // free(msg);
-    PORTB |= _BV(PB6); // 0b01000000
-    SREG=cSREG; //restore SREG
-}
-
-// TODO: change this method for each of the demo nodes
-void handleCANmsg(uint8_t destID, uint8_t msgID, uint8_t* msg, uint8_t msgLen) {
-    uint8_t cmd = *msg;
-    PORTB = cmd;
+void UART_putChar(char c) {
+    while(LINSIR & _BV(LTXOK)); // controller is busy
+    LINDAT = c;
 }
 
 int main (void) {
-    DDRB |= 0xFF; // set all PORTB pins for output
-    DDRD &= ~(_BV(PD6)); // set pin 14 for input
+    DDRD &= ~(_BV(PD4)); // set pin 12 for input
+    DDRD |= _BV(PD3); // set pin 2 for output
 
     sei(); // enable global interrupts    
-    // initCAN(NODE_demoServer); // initialize CAN bus
-    initButton(); // intitialize button interrupts
+    initUART(); // intitialize button interrupts
 
     for (;;) {
-        // listen for button presses forever
+        // spit out debug messages forever
+        UART_putChar('a');
+        _delay_ms(50);
     }
+
+    return(0);
 }
 

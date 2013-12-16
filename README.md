7cd9c1dc71c553245f9bc24cf8e65621d46d8f2e
Derek Redfern <redfern.derek@gmail.com>
2013-12-16 00:20:48 -0500

    Split the UART code out into a standalone library file. Adding a PC-side viewing script. Updated README.

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 AVR-UART
 ========
 
-Getting the UART port working on the ATMega16M1s for debugging - REVO Buildathon Fall 2013
+Debugging via UART - REVO Buildathon Fall 2013
+
+Setting up the hardware
+-----------------------
+1. Obtain an FTDI cable. The one REVO has is on page 10 of this document http://www.ftdichip.com/Support/Documents/DataSheets/Cables/DS_TTL-232R_CABLES.pdf and a picture is here http://makeyourbot.wdfiles.com/local--files/fabio-1-1/ftdi.jpg
+2. Plug the black wire (GND) into ground on your circuit.
+3. DO NOT plug in the red wire (POWER) unless you do not have another power source. If you plug them both in, they will both be sad.
+4. Plug the yellow wire (RXD) into pin 2 of the ATMega16M1 (TXD).
+5. Plug the USB into your computer.
+
+Setting up the software
+-----------------------
+Usage:
+1. Put uart.c and uart.h in your src/ folder.
+2. Put the following in any file where you want to use UART: #include "uart.h"
+3. Add the following to your Makefile (this may need to be modified depending on your Makefile configuration):
+SOURCES += uart.c
+INC += -Isrc/
+CFLAGS += $(INC)
+
+4. Call initUART to turn on UART functionality.
+5. Call UART_putString to send a message.
+6. Call endUART to turn off UART functionality. (It doesn't hurt anything to leave it on, so you shouldn't need this.)
+
+See main.c in this folder for an example of how to debug using these functions.
+
+Viewing messages
+----------------
+Run the following commands in the folder containing view_uart_script:
+
+chmod +x view_uart_script
+./view_uart_script
--- main.c
+++ main.c
@@ -2,123 +2,14 @@
         Initializes the UART controller and sends out sporadic debug messages.
 */
 
-#define F_CPU (1000000L)
-#define UART_BAUD (9600L)
-#define UART_SAMPLERATE (52UL)
-#define BAUD_PRESCALE (((F_CPU / (UART_BAUD * UART_SAMPLERATE))) - 1)
-#include <avr/io.h>
-#include <util/delay.h>
-#include <inttypes.h>
-#include <avr/interrupt.h>
-#include <stdlib.h>
-
-#define MAX_TX_BUFFER_SIZE 200 // maximum size of transmit buffer in bytes
-
-// define pointers for the circular buffer
-// This works by defining a buffer and writing characters to be transmitted
-//  into it via the write pointer. The characters are then transmitted them
-//  one at a time via the read pointer. If the write pointer passes the end
-//  pointer, it loops around to the begin pointer again. If the write pointer
-//  meets the read pointer, it returns with an error.
-char* TX_begin;
-char* TX_end;
-char* TX_read;
-char* TX_write;
-
-int transmitting;
-
-// Set up the LIN/UART Controller
-// RX on 12
-// TX on 2
-
-// Scale down to 2400 baud (bits per sec)
-// 
-void initUART(void) {
-    // set up the transmit buffer
-    TX_begin = (char*)malloc(MAX_TX_BUFFER_SIZE*sizeof(char));
-    TX_end = TX_begin + MAX_TX_BUFFER_SIZE*sizeof(char);
-    TX_read = TX_begin;
-    TX_write = TX_begin;
-
-    transmitting = 0;
-
-    // initialize pins and interrupts
-    sei(); // enable global interrupts
-    DDRD &= ~(_BV(PD4)); // set pin 12 for input
-    DDRD |= _BV(PD3); // set pin 2 for output
-
-    // reset the controller and activate UART
-    LINCR = _BV(SWRES) | _BV(LENA) | _BV(LCMD2) | _BV(LCMD0);
-    LINBTR = 32;
-    LINBRR = 12; // baud rate prescaler
-    LINENIR |= _BV(LENTXOK);
-}
-
-// turn off the UART controller and free the buffer memory
-void endUART(void) {
-    transmitting = 0;
-    LINCR = _BV(SWRES);
-    free(TX_begin);
-}
-
-// transmission has completed successfully
-// load in next byte if one exists
-ISR(LIN_TC_vect) {
-    PORTB ^= _BV(PB7);
-    // _delay_ms(50);
-
-    if (TX_read != TX_write) {
-        LINDAT = *TX_read;
-        TX_read++;
-        if (TX_read == TX_end) {
-            TX_read = TX_begin;
-        }
-    } else {
-        transmitting = 0;
-        LINSIR |= _BV(LTXOK);
-    }
-}
-
-int UART_transmit() {
-    if (transmitting != 1) {
-        if (TX_read != TX_write) {
-            PORTB ^= _BV(PB7);
-            // _delay_ms(50);
-            transmitting = 1;
-            LINDAT = *TX_read;
-            TX_read++;
-            if (TX_read == TX_end) {
-                TX_read = TX_begin;
-            }
-        }
-    }
-    return(0);
-}
-
-int UART_putString(char s[], int len) {
-    int i;
-    for (i=0;i<len;i++) {
-        // add *(s+i) to the circular buffer
-        *TX_write = s[i];
-
-        TX_write++;
-        if (TX_write == TX_end) {
-            TX_write = TX_begin;
-        }
-    }
-    UART_transmit();
-    return(0);
-}
+#include "uart.h"
 
 int main (void) {
-    DDRB |= _BV(PB7);
-
     initUART(); // intitialize UART controller
-    char testMsg[] = "Hello, World!\n";
     for (;;) {
         // spit out debug messages forever
         _delay_ms(500);
-        UART_putString(testMsg,14);
+        UART_putString("Hello, World!\n",14);
     }
 
     return(0);
